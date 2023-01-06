ffb373743cb73d8200aaffb4242b9f3c20f8a990
mytortoise <69615078+HookHookUK@users.noreply.github.com>
2023-01-06 18:01:24 +0900

    2141
    afds

--- Assets/TurnSystem.cs
+++ Assets/TurnSystem.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
 
 public enum OrderType
 {
@@ -16,142 +18,279 @@ public enum OrderType
     End
 }
 
-public class TurnSystem : MonoBehaviour
+public class TurnSystem : MonoBehaviourPunCallbacks
 {
-    bool isGameOver;
-    [SerializeField] GameObject player;
-    [SerializeField] GameObject other;
+    private bool isGameOver;
 
-    [SerializeField] GameObject[] playerCardPos;
-    [SerializeField] GameObject[] otherCardPos;
 
-    private void Awake()
+
+    [SerializeField] private GameObject Me;
+    [SerializeField] private GameObject enemyDeck;
+
+    [SerializeField] private GameObject[] playerCardPos;
+    [SerializeField] private GameObject[] otherCardPos;
+
+    public Player[] players = null;
+
+    [SerializeField] public InputField inputField; // 닉네임 입력칸
+
+    public int[] setRandom = new int[500]; // 공격할 대상을 랜덤으로 지정한다. 
+
+    public int userID = 0;
+    /*    private TurnSystem instance = null;
+        public TurnSystem Inst
+        {
+            get
+            {
+                if(instance == null)
+                    instance = FindObjectOfType<TurnSystem>();
+                return instance;
+            }
+        }*/
+
+    #region 포톤 콜백 함수들
+
+    public void SetName()
     {
-        playerCardPos = new GameObject[6];
-        otherCardPos = new GameObject[6];
+        string myName = null;
+        PhotonNetwork.NickName = myName;
     }
 
 
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("마스터서버에 연결 성공");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("로비에 입장 성공");
+
+
+        PhotonNetwork.JoinRoom("test");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        // 룸옵션에 대한 정보
+        RoomOptions roomOptions = new RoomOptions() { MaxPlayers = 20 };
+        PhotonNetwork.CreateRoom("test", roomOptions);
+        //PhotonNetwork.JoinRoom("test");
+        Debug.Log("test룸 생성");
+    }
+
+
+
+    public override void OnJoinedRoom() // 자기 자신만
+    {
+        Debug.Log("test룸 입장");
+
+        // 플레이어 고유번호 지정 ( 랜덤값으로 선공을 설정하기 위함 )
+        players = PhotonNetwork.PlayerList;
+        Debug.Log("방 접속자 수 : " + PhotonNetwork.PlayerList.Length);
+        Debug.Log(players[0].ActorNumber);
+        for (int i = 0; i < players.Length; i++)
+        {
+            PhotonNetwork.NickName = PhotonNetwork.PlayerList[i].ActorNumber.ToString();
+            if (PhotonNetwork.PlayerList[i].NickName == PhotonNetwork.NickName) userID = i;  // 고유닉네임이 일치하면 고유번호를 지정한다.
+        }
+        /*for(int i = 0; i < players.Length; i++)
+        {
+            // 고유 번호를 매긴다.
+            if (players[i].ActorNumber == playerNum)
+            {
+                playerID = i;
+                Debug.Log("내 ID는 " + i);
+                playerNum++;
+            }
+        }*/
+
+    }
+    #endregion
+
+    public override void OnPlayerEnteredRoom(Player newPlayer) // 다른 사람 입장시
+    {
+        Debug.Log(newPlayer.NickName + " 입장");
+        Debug.Log("방 접속자 수 : " + PhotonNetwork.PlayerList.Length);
+    }
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings(); // 포톤 기본세팅 사용 및 마스터서버 연결
+    }
+
     private void Start()
     {
+        //playerCardPos = new GameObject[6];
+        //otherCardPos = new GameObject[6];
         GameLoop();
     }
 
     private void GameLoop()
     {
-        GameStart(); // 게임이 시작되면 최초 한번만 실행
 
         BattleOrder(); // 순서대로 전투
 
-        if(!isGameOver)
-        GoShop(); // 상점으로 가!
+        if (!isGameOver)
+            GoShop(); // 상점으로 가!
     }
 
-    void GameStart()
+    //List<Player> matchMan = null;
+    Player[] matchMan = new Player[8]; // 최대 8인이고 그 이상을 넘을 수는 없으니 일단 이 값으로 지정.
+
+    [PunRPC]
+    public void MatchingSetting()
     {
-        // 상대방과 자신의 덱 동기화
-        int whoFirst = Random.Range(0, 2);
-        if(whoFirst == 0)
-        {
-            // 0번째가 1번
-        }
-        else
-        {
-            // 1번째가 1번
+        // 매칭은 마스터클라이언트 혼자 관리하여 전역으로 실행한다.
 
+        Queue<Player> matchQueue = new Queue<Player>();
+
+        for (int i = 0; i < setRandom.Length; i++)
+        {
+            setRandom[i] = Random.Range(0, 3);
         }
-        // 선공 후공 설정
+        int n = 0;
+        // 큐에 플레이어들을 랜덤한 순서대로 집어넣는다.
+
+        players = PhotonNetwork.PlayerList;
+        Debug.Log("players.Length 의 길이는 :" + players.Length);
+
+
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            n = Random.Range(0, PhotonNetwork.PlayerList.Length);
+                if (matchQueue.Contains(players[n]))
+                { i--; continue; }
+                matchQueue.Enqueue(players[n]);
+                Debug.Log("zz");
+            
+        }
+        Debug.Log(matchQueue.Count);
+        // 플레이어들을 큐에 다 집어넣으면 배열에 순서대로 넣는다.
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            matchMan[i] = matchQueue.Dequeue();
+        }
+
+        // 모든 세팅이 끝났다.
+        photonView.RPC("Matching", RpcTarget.AllViaServer, setRandom, matchMan);
+
     }
 
-    void GoShop()
+    [PunRPC]
+    public void Matching(int[] random, Player[] matchMan)
+    {
+        //마스터 클라이언트가 대진 설정을 마치고 각 플레이어들을 1:1로 묶는 함수
+
+        if (PhotonNetwork.PlayerList.Length < 2)
+        {
+            // 나 혼자 남았으니 자동 승리
+            Debug.Log("나 혼자 방에 남아버렸다");
+        }
+
+        else if (PhotonNetwork.PlayerList.Length == 2)
+        {
+            // 1 : 1 시작
+            if (matchMan[0].NickName == PhotonNetwork.NickName)
+            {
+                // 내가 선공
+                Debug.Log("나는 " + userID + " 닉네임 : " + matchMan[0].NickName + " 상대 : " + matchMan[1].NickName);
+                // 내 상대는 배열1
+
+            }
+            else
+            {
+                // 내가 후공
+                Debug.Log("나는 " + userID + " 닉네임 : " + matchMan[1].NickName + " 상대 : " + matchMan[0].NickName);
+                // 내 상대는 배열0
+            }
+        }
+
+        // 플레이어가 짝수, 4명 이상이라면
+        else if (PhotonNetwork.PlayerList.Length % 2 == 0 && PhotonNetwork.PlayerList.Length > 3)
+        {
+            for (int i = 0; i < players.Length;)
+            {
+                if (matchMan[i].NickName == PhotonNetwork.NickName)
+                {
+                    if (i == 0 || i % 2 == 0)
+                    {
+                        // 내 상대는  = matchMan[i+1] 
+                        Debug.Log("나는 " + userID + " 닉네임 : " + matchMan[0].NickName + " 상대 : " + matchMan[1].NickName);
+                        // 내가 선공
+                    }
+                    else
+                    {
+                        // 내 상대는 = matchMan[i-1]
+                        Debug.Log("나는 " + userID + " 닉네임 : " + matchMan[1].NickName + " 상대 : " + matchMan[0].NickName);
+                        // 내가 후공
+                    }
+                }
+            }
+        }
+
+        // 플레이어가 홀수, 3명 이상이라면
+        else if (PhotonNetwork.PlayerList.Length % 2 == 1 && PhotonNetwork.PlayerList.Length > 2)
+        {
+            for (int i = 0; i < players.Length;)
+            {
+                if (matchMan[i].NickName == PhotonNetwork.NickName)
+                {
+                    if (i == 0 || i % 2 == 0)
+                    {
+                        // 내 상대는  = matchMan[i+1]
+                        Debug.Log("나는 " + userID + " 닉네임 : " + matchMan[0].NickName + " 상대 : " + matchMan[1].NickName);
+                    }
+                    else
+                    {
+                        // 내 상대는 = matchMan[i-1]
+                        Debug.Log("나는 " + userID + " 닉네임 : " + matchMan[1].NickName + " 상대 : " + matchMan[0].NickName);
+                    }
+                }
+            }
+        }
+    }
+
+    private void GoShop()
     {
         // 상점 씬으로 이동
+
     }
 
-    public void OnBuy()
-    {
-        Debug.Log("OnBuy");
-    }
-
-    public void OnSell()
-    {
-        Debug.Log("OnSell");
-    }
-
-    void BattleOrder()
+    private void BattleOrder()
     {
         // 패시브(순서 상관없이 항상 발동) // 함수 X
-        
-        // 전투 시작시 - 글로벌
-        OnStart();
 
-        // 공격 시작 //
-        for(int i = 0; i < 6; i++)
-        {
-            
-        }
-        // 공격시 == 공격을 성공했을 때
-        OnAttack();
-        // 피격시
-        OnHit();
-
-        // 처치시
-        OnKill();
-        // 사망시 
-        OnDie();
-        
-        // 턴 종료시 - 글로벌
-        OnEnd();
-
-        // 생존시 - 글로벌 
-        OnAlive();
-    }
-
-    void ProcessOrder(OrderType orderType)
-    {
+        // 전투 시작
+        // if 내가 선공이라면
         for (int i = 0; i < 6; i++)
         {
-            //1번째 부터 각 자리에 위치한 카드의 Onstart()를 호출한다.
-            
-            
+            // myDeck.transform.GetChild(i).gameObject.GetComponent<Card>().OnStart();
+            // otherDeck.transform.GetChild(i).gameObject.GetComponent<Card>().OnStart();
+        }
+        // else 상대가 선공이라면
+        for (int i = 0; i < 6; i++)
+        {
+            // otherDeck.transform.GetChild(i).gameObject.GetComponent<Card>().OnStart();
+            // myDeck.transform.GetChild(i).gameObject.GetComponent<Card>().OnStart();
+        }
+
+        // 턴 종료 = 상점으로 이동
+        GoShop();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            photonView.RPC("MatchingSetting", RpcTarget.MasterClient);
         }
     }
-
-
-    void OnStart()
-    {
-        Debug.Log("OnStart");
-
-    }
-
-    void OnAttack()
-    {
-        Debug.Log("OnAttack");
-    }
-
-    void OnHit()
-    {
-        Debug.Log("OnHit");
-    }
-
-    void OnKill()
-    {
-        Debug.Log("OnHit");
-    }
-
-    void OnDie()
-    {
-        Debug.Log("OnDie");
-    }
-
-    void OnAlive()
-    {
-        Debug.Log("OnAlive");
-    }
-
-    void OnEnd()
-    {
-        Debug.Log("OnEnd");
-    }
-
 }
